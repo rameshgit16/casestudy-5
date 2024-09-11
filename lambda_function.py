29c4ef87bf7c4cc6b1c90be3affcfc03c2e2c37b
rameshgit16 <mo.ramesh16@gmail.com>
2024-09-11 14:44:15 +0530

    Update and rename lambda_function_1.py to lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import boto3
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name='your-region')  # Replace 'your-region' with the correct region
+    
+    # Get list of all stopped instances
+    instances = ec2.describe_instances(Filters=[{
+        'Name': 'instance-state-name',
+        'Values': ['stopped']
+    }])
+    
+    # Extract instance IDs
+    instance_ids = [instance['InstanceId'] for reservation in instances['Reservations'] for instance in reservation['Instances']]
+    
+    if instance_ids:
+        # Start the instances
+        ec2.start_instances(InstanceIds=instance_ids)
+        print(f'Starting instances: {instance_ids}')
+    else:
+        print('No stopped instances found')
+
+    return {
+        'statusCode': 200,
+        'body': 'Instances started successfully'
+    }
