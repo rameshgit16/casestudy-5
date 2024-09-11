4f67cede20c1657e3133934fe50c0bc7f3acd1ed
rameshgit16 <mo.ramesh16@gmail.com>
2024-09-11 14:54:20 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,26 @@
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='ap-south-1')
+    ec2 = boto3.client('ec2', ap-south-1')  # Replace 'your-region' with the correct AWS region
     
-    # Get list of all running instances
+    # Get list of all stopped instances
     instances = ec2.describe_instances(Filters=[{
-        'Name': 'instance-state-name', 
-        'Values': ['running']
+        'Name': 'instance-state-name',
+        'Values': ['stopped']
     }])
     
     # Extract instance IDs
     instance_ids = [instance['InstanceId'] for reservation in instances['Reservations'] for instance in reservation['Instances']]
     
     if instance_ids:
-        # Stop the instances
-        ec2.stop_instances(InstanceIds=instance_ids)
-        print(f'Stopping instances: {instance_ids}')
+        # Start the instances
+        ec2.start_instances(InstanceIds=instance_ids)
+        print(f'Starting instances: {instance_ids}')
     else:
-        print('No running instances found')
-    
-    # Properly formatted return statement
+        print('No stopped instances found')
+
+    # Return a response after starting instances
     return {
         'statusCode': 200,
-        'body': 'Instances stopped successfully'
+        'body': f'Instances started successfully: {instance_ids}' if instance_ids else 'No stopped instances found to start'
     }
